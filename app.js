599f64881e9a727ec698ebaff267d4e45cb2f512
Kaisma Penn-Titley <kptitley@gmail.com>
2017-09-15 22:29:30 -0400

    hello world express app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(request, response) {
+    response.send("Hello World!");
+});
+
+app.listen(3001, function() {
+    console.log("Example app litening on port 3001!");
+});
